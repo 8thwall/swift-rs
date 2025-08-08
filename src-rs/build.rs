c4ce57b947e97aac9e7157f676d2610e57840c60
Paris Morgan <paris@8thwall.com>
2025-08-08 14:05:42 -0700

    Disable sandboxing of Swift build to fix errors when building in a Bazel sandbox

--- src-rs/build.rs
+++ src-rs/build.rs
@@ -302,7 +302,7 @@ impl SwiftLinker {
             command
                 // Build the package (duh)
                 .arg("build")
-                // NOTE(paris): Disable the sandbox because we build inside the Bazel sandbox, which 
+                // NOTE(paris): Disable the sandbox because we build inside the Bazel sandbox, which
                 // has its own sandbox. Without this we fail with:
                 // - `sandbox-exec: sandbox_apply: Operation not permitted`
                 .arg("--disable-sandbox")
